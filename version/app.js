a8664ddab3aba744527b4775855e990547b8fe6e
macdonn21 <nnamdi@uwindsor.ca>
2020-10-29 11:37:01 -0400

    update

--- version/app.js
+++ version/app.js
@@ -5,7 +5,7 @@ var response;
 
 app.get('/', function (req, res) {
 
-    response = 'This is Mcdonald Version.' + '\n';
+    response = 'This is  a Mcdonalds Test Version.' + '\n';
 
     //send the response to the client
     res.send(response);
